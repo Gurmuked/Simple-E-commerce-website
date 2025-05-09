8466dc332d73d644ef7d34ba28917334bebb2bad
Gurmesa <gurmesakedir96@gmail.com>
2025-05-10 00:50:54 +0300

    File setup or installation

---
+++ ECom/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+   <h1 className='text-8xl text-blue-950 '>Hello Gking</h1>
+    </>
+  )
+}
+
+export default App;
