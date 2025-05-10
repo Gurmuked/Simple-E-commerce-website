33cb0471093147188013b5e94da3ef934aac0739
Gurmesa <gurmesakedir96@gmail.com>
2025-05-11 00:53:33 +0300

    basic structure for Inspiration and Products components.

--- ECom/src/components/Inspiration.jsx
+++ ECom/src/components/Inspiration.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Button from './Button';
 
-const Inspiration = () => {
-  return (
-    <div>Inspiration</div>
+const Inspiration = () =>(
+    <section>
+      <div>
+        <h3><span>50+</span> Beautiful rooms inspiration</h3>
+        <p>Our designer already made a lot of beautiful prototype of rooms that inspire you</p>
+        <Button value="Explore More" style="bg-yellow-600 text-white"/>
+      </div>
+      
+      <div></div>
+      <div></div>
+    </section>
   )
-}
 export default Inspiration ;
--- ECom/src/components/Products.jsx
+++ ECom/src/components/Products.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Button from './Button';
 
- const Products = () => {
-  return (
-    <div>Products</div>
+ const Products = () =>(
+    <section>
+      <div>
+
+        <h1>Our Products</h1>
+        <div></div>
+        <Button value="Show More" style="bg-white text-yellow-700 border-1 border-yellow-700" />
+
+      </div>
+    </section>
   )
-}
 export default Products;
