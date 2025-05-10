5e3d76748cfae46ac5d38fe8fa729ee9143d3afe
Gurmesa <gurmesakedir96@gmail.com>
2025-05-11 00:52:26 +0300

    create array gallery and create index js for simple import all other compoents

---
+++ ECom/src/components/index.js
@@ -0,0 +1,15 @@
+import Hero from './Hero';
+import Category from './Category';
+import Products from './Products';
+import Inspiration from './Inspiration';
+import Arrangement from './Arrangement';
+import Footer from './Footer';
+
+export {
+  Hero,
+  Category,
+  Products,
+  Inspiration,
+  Arrangement,
+  Footer
+};
---
+++ ECom/src/constants/Gallery.js
@@ -0,0 +1,72 @@
+import { barber, bedroom, bedroom1, mall, mealplace, mealplace1, restplace, salon, salon1, salon2, salon3, salon4 } from '../assets';
+
+export const category = [
+  {
+    img: mealplace,
+    title: "Dinig"
+  },
+   {
+    img: bedroom,
+    title: "Living"
+  },
+   {
+    img: restplace,
+    title: "Bedroom"
+  }
+];
+
+export const products = [
+   {
+    img: salon,
+    title: "Syitherine",
+    subtitle: "Stylish cafe ehair",
+    price: 2500.000,
+    delPrice: 3600.000
+  },
+  {
+    img: salon1,
+    title: "Leviosa",
+    subtitle: "Stylish cafe ehair",
+    price: 2500.000,
+  },
+  {
+    img: salon2,
+    title: "Lolito",
+    subtitle: "Luxury big safe",
+    price: 2500.000,
+    elPrice: 14000
+  },
+  {
+    img: salon3,
+    title: "Respira",
+    subtitle: "outdoor bar table and stool",
+    price: 2500.000,
+  },
+  {
+    img: salon4,
+    title: "Grifo",
+    subtitle: "Might lamp",
+    price: 1500.000,
+  },
+  {
+    img: salon1,
+    title: "Muggo",
+    subtitle: "Small mug",
+    price: 150.000,
+  },
+  {
+    img: salon2,
+    title: "Pingky",
+    subtitle: "Cute beal set",
+    price: 7000.000,
+    delPrice: 14000.000
+  },
+  {
+    img: salon3,
+    title: "Potty",
+    subtitle: "Minimalist flower pot",
+    price: 500.000,
+  }
+]
+
+export const inspiration = [bedroom1, barber, mall]
