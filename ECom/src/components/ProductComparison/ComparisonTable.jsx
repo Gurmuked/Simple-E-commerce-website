d695537084524f8bdd60a0b3678171eadbfdd565
Gurmesa <gurmesakedir96@gmail.com>
2025-06-13 20:28:47 +0300

    componenets that contain infromations

---
+++ ECom/src/components/ProductComparison/ComparisonTable.jsx
@@ -0,0 +1,56 @@
+  export const products = [
+    {
+      id: 1,
+      name: "Asgaard Sofa",
+      image: "../src/assets/sofa1.png",
+      rating: 4.5,
+      reviews: 12,
+      link: "/product",
+      addLabel: "Add to Cart",
+    },
+    {
+      id: 2,
+      name: "Outback Sofa Set",
+      image: "../src/assets/sofa2.png",
+      rating: 4.0,
+      reviews: 8,
+      link: "/product",
+      addLabel: "Add to Cart",
+    },
+  ]
+
+  export const sections = [
+    {
+      title: "General",
+      rows: [
+        { label: ["Sales Package", "Model Number", "Secondary Material", "Configuration", "Upholstery Material", "Upholstery Color"]},
+        { label: ["1 sectional sofa", "TFCBLIGRBL6SRHS", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"] },
+        { label: ["1 Three Seater, 2 Single Seater", "DTUBLIGRBL568", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"]}
+      ],
+    },
+    {
+      title: "Product",
+      rows: [
+        { label: ["Sales Package", "Model Number", "Secondary Material", "Configuration", "Upholstery Material", "Upholstery Color"]},
+        { label: ["1 sectional sofa", "TFCBLIGRBL6SRHS", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"] },
+        { label: ["1 Three Seater, 2 Single Seater", "DTUBLIGRBL568", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"]},
+      ],
+    },
+    {
+      title: "Dimensions",
+      rows: [
+        { label: ["Sales Package", "Model Number", "Secondary Material", "Configuration", "Upholstery Material", "Upholstery Color"]},
+        { label: ["1 sectional sofa", "TFCBLIGRBL6SRHS", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"] },
+        { label: ["1 Three Seater, 2 Single Seater", "DTUBLIGRBL568", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"]},
+      ],
+    },
+    {
+      title: "Warranty",
+      rows: [
+        { label: ["Sales Package", "Model Number", "Secondary Material", "Configuration", "Upholstery Material", "Upholstery Color"]},
+        { label: ["1 sectional sofa", "TFCBLIGRBL6SRHS", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"] },
+        { label: ["1 Three Seater, 2 Single Seater", "DTUBLIGRBL568", "Solid Wood", "L-shaped", "Fabric + Cotton", "Bright Grey & Lion"]},
+      ],
+    },
+  ]
+
