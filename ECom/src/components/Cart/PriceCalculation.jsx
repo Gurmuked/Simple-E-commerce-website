37f9c3d149bcd097cc90abda477c5ef272338b36
Gurmesa <gurmesakedir96@gmail.com>
2025-06-14 22:53:23 +0300

    price calculation in the cart components  total price

--- ECom/src/components/Cart/PriceCalculation.jsx
+++ ECom/src/components/Cart/PriceCalculation.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const PriceCalculation = () => {
   return (
-    <div class='w-[400px] h-[300px] bg-beige' >PriceCalculation</div>
+    <selection className='flex flex-col items-center w-[400px] h-[300px] bg-beige mt-10 mb-10 mx-10 py-8 ml-4' >
+      <h4 className="font-poppins font-semibold text-[24px] pb-4 mb-6">Cart Totals</h4>
+      <div className="flex justify-left mb-4 ">
+        <p className="font-poppins font-semibold text-[16px]">Subtotal &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </p>
+        <p className="font-poppins text-[14px] text-gray-600">Rs. 250,000.00</p>
+      </div>
+      <div className="flex justify-left mb-4 ">
+        <p className="font-poppins font-semibold text-[16px]">Total &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </p>
+        <p className="font-poppins text-[14px] text-yellow-600">Rs. 250,000.00</p>
+      </div>
+      <Link to="/checkout">
+        <button className="border-2 rounded-lg py-2 px-6 border-gray-400 text-gray-700 mt-4">Check Out</button>
+      </Link>
+    </selection>
   )
 }
 
--- ECom/src/components/Cart/ProductDetail.jsx
+++ ECom/src/components/Cart/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { AiOutlineDelete } from "react-icons/ai";
 
 
 const row1= ["Product", "Price", "Quantity", "subtotal"]
@@ -6,20 +6,20 @@ const row2= ["ASgaad sofa", "Rs. 25,000", 1, "Rs. 25,00"]
 
 const ProductDetail = () => {
   return (
-    <section className='flex flex-col justify-between'>
-      <div className='flex  text-center my-10'>
+    <section className='w-[100%] flex flex-col justify-between'>
+      <div className='flex justify-center bg-beige text-center my-10 w-[100%] h-[60px] items-center'>
         {
-        row1.map((item) =>
-        <div className='mx-[34px]'>{item}</div>
+        row1.map((item, index) =>
+        <p key={index} className='mx-[60px]'>{item}</p>
         )}
       </div>
-      <div className='grid grid-flow-col 6 ml-20 mr-20 mb-40'>
+      <div className='grid grid-flow-col 6 mx-10 mb-40 w-[100%]'>
         <img src="../src/assets/logo.png" alt="" />
         {
-        row2.map((item) =>
-        <p>{item}</p>
+        row2.map((item, index) =>
+        <p key={index}>{item}</p>
         )}
-        <img src="../src/assets/logo.png" alt="" />
+        <AiOutlineDelete className="text-yellow-600 w-8 h-8"/>
       </div>
     </section>
   )
