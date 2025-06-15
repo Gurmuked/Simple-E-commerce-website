0e4c14bbde5698bb643d182b1998b60e2e360dd1
Gurmesa <gurmesakedir96@gmail.com>
2025-06-15 23:46:43 +0300

    this is All about important information important in About us page

---
+++ ECom/src/components/About/Data.jsx
@@ -0,0 +1,51 @@
+export const Data = [
+  {
+    img: "../.././src/assets/Rect68.png",
+    title:"Going all-in with millennial design",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
+  },
+  {
+    img: "../.././src/assets/Rect681.png",
+    title:"Exploring new ways of decorating",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
+  },
+  {
+    img: "../.././src/assets/Rect682.png",
+    title:"Handmade pieces that took time to make",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
+  }
+];
+
+
+export  const posts = [
+    {
+      id: "Rect69",
+      title: "Going all-in with\nmillennial design",
+      date: "Aug 2022",
+      image: "../.././src/assets/Rect69.png"
+    },
+    {
+      id: "Rect691",
+      title: "Exploring new ways\nof decorating",
+      date: "Aug 2022",
+      image: "../.././src/assets/Rect691.png"
+    },
+    {
+      id: "Rect692",
+      title: "Handmade pieces\nthat took time to make",
+      date: "Aug 2022",
+      image: "../.././src/assets/Rect692.png"
+    },
+    {
+      id: "Rect693",
+      title: "Modern home in\nMilan",
+      date: "Aug 2022",
+      image: "../.././src/assets/Rect693.png"
+    },
+    {
+      id: "Rect694",
+      title: "Colorful office\nredesign",
+      date: "Aug 2022",
+      image: "../.././src/assets/Rect694.png"
+    },
+  ];
